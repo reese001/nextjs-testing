e1e8e42b2ba3d8dd5633b3409494cd46e9ff2ef0
s3anmorrow <s3anmorrow@gmail.com>
2024-09-17 11:38:48 -0300

    Banner component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 
-// importing google font for NextJS
-import { Griffy } from 'next/font/google';
-const griffy = Griffy({weight: "400", subsets: ['latin']});
+import Banner from "@/components/Banner";
 
 export default function Home() {
   // retrieve server sided script
@@ -15,21 +13,7 @@ export default function Home() {
   return (
     <main className="grid grid-rows-1 grid-cols-1 gap-0 text-content">
 
-      <div className="flex flex-nowrap items-center justify-center 
-          bg-accent bg-[url('./../lib/images/background.jpg')] bg-no-repeat bg-center bg-cover
-          border-solid border-b-4 border-accent min-h-[220px] p-5 text-white">
-
-        <header className="grow text-center md:text-left">
-          <div className={`${griffy.className} text-6xl`}>Antonio&apos;s Online Pizzaria</div>
-          <div className="text-sm">If it&apos;s not Antonio&apos;s, it&apos;s rubbish!</div>
-        </header>
-
-        <div className="shrink-0 hidden md:block">
-          <i className="fab fa-facebook-square fa-2x ml-1"></i>
-          <i className="fab fa-twitter-square fa-2x ml-1"></i>
-          <i className="fab fa-instagram fa-2x ml-1"></i>
-        </div>
-      </div>
+      <Banner />
 
       <aside className="flex flex-nowrap items-center justify-between p-5 flex-col md:flex-row">
         <div className="mb-5 md:hidden text-center">
---
+++ src/components/Banner.tsx
@@ -0,0 +1,23 @@
+// importing google font for NextJS
+import { Griffy } from 'next/font/google';
+const griffy = Griffy({weight: "400", subsets: ['latin']});
+
+export default function Banner() {
+    return (
+        <div className="flex flex-nowrap items-center justify-center 
+          bg-accent bg-[url('./../lib/images/background.jpg')] bg-no-repeat bg-center bg-cover
+          border-solid border-b-4 border-accent min-h-[220px] p-5 text-white">
+
+        <header className="grow text-center md:text-left">
+          <div className={`${griffy.className} text-6xl`}>Antonio&apos;s Online Pizzaria</div>
+          <div className="text-sm">If it&apos;s not Antonio&apos;s, it&apos;s rubbish!</div>
+        </header>
+
+        <div className="shrink-0 hidden md:block">
+          <i className="fab fa-facebook-square fa-2x ml-1"></i>
+          <i className="fab fa-twitter-square fa-2x ml-1"></i>
+          <i className="fab fa-instagram fa-2x ml-1"></i>
+        </div>
+      </div>
+    );
+}
