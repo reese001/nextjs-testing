25c36fa675b2cff7d3183c6fb7d9e68a2bf4a986
s3anmorrow <s3anmorrow@gmail.com>
2024-09-17 15:55:07 -0300

    updated title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "boilerplate-nextjs",
-  description: "The boilerplate starting project folder for PROG3017",
+  title: "Pizzaria Delivery Web App",
+  description: "A demonstration of Client Side Data Fetching for PROG3017",
 };
 
 export default function RootLayout({ children }:Readonly<{ children: React.ReactNode }>) {
