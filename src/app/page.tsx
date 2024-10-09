ec279e6d719d8115f2e3bd781e034ca1974b387f
reese parsons <reeseparsons99@gmail.com>
2024-10-09 12:20:03 -0300

    added order functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,67 @@
 
 import Banner from "@/components/Banner";
 import OrdersReport from "@/components/OrdersReport";
+import { useState } from "react";
+import LoadingOverlay from "@/components/LoadingOverlay";
 
 export default function Home() {
+    // ---------------------------- event handlers
+    const showOrders = (e: any) => setAppState(2);
 
-  // ---------------------------- rendering to DOM
-  return (
-    <main className="grid grid-rows-1 grid-cols-1 gap-0 text-content">
+    // ---------------------------- state variables
+    // 1 - no orders loaded
+    // 2 - orders loading
+    // 3 - orders have been loaded
+    const [appState, setAppState] = useState(1);
 
-      <Banner />
+    // ---------------------------- rendering to DOM
+    return (
+        <main className="grid grid-rows-1 grid-cols-1 gap-0 text-content">
+            <LoadingOverlay
+                show={appState == 2 ? true : false}
+                bgColor="#b82308"
+            />
 
-      <aside className="flex flex-nowrap items-center justify-between p-5 flex-col md:flex-row">
-        <div className="mb-5 md:hidden text-center">
-          <>1234 Cheesy Drive | Tastyville, NS | 902-123-4567</>
-        </div>
-        <div>
-          <div className="text-accent text-3xl font-bold mb-2.5">Welcome loyal pizza dispatcher....</div>Click the &quot;Get Orders&quot; button below to view all current orders that need to be delivered.
-          <div>
-              <button 
-                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5">Get Orders</button>
-          </div>
-        </div>
-        <div className="shrink-0 text-lg text-right text-greyContent hidden md:block">
-          <div>Antonio&apos;s Pizzaria</div>
-          <div>1234 Cheesy Drive</div>
-          <div>Tastyville, NS</div>
-          <div>902-123-4567</div>
-        </div>
-      </aside>
+            <Banner />
 
-      <div className="bg-greyAccent p-10">
+            <aside className="flex flex-nowrap items-center justify-between p-5 flex-col md:flex-row">
+                <div className="mb-5 md:hidden text-center">
+                    <>1234 Cheesy Drive | Tastyville, NS | 902-123-4567</>
+                </div>
+                <div>
+                    <div className="text-accent text-3xl font-bold mb-2.5">
+                        Welcome loyal pizza dispatcher....
+                    </div>
+                    Click the &quot;Get Orders&quot; button below to view all
+                    current orders that need to be delivered.
+                    <div>
+                        <button
+                            className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5"
+                            onClick={showOrders}
+                        >
+                            Get Orders
+                        </button>
+                    </div>
+                </div>
+                <div className="shrink-0 text-lg text-right text-greyContent hidden md:block">
+                    <div>Antonio&apos;s Pizzaria</div>
+                    <div>1234 Cheesy Drive</div>
+                    <div>Tastyville, NS</div>
+                    <div>902-123-4567</div>
+                </div>
+            </aside>
 
-        <div id="output" className="divide-dashed divide-y-2 divide-accent">
-
-          <OrdersReport />
-
-        </div>
-      </div>
-    </main>
-  );
+            <div className="bg-greyAccent p-10">
+                <div
+                    id="output"
+                    className="divide-dashed divide-y-2 divide-accent"
+                >
+                    <OrdersReport
+                        setAppState={setAppState}
+                        appState={appState}
+                    />
+                </div>
+            </div>
+        </main>
+    );
 }
--- src/components/OrdersReport.tsx
+++ src/components/OrdersReport.tsx
@@ -1,12 +1,42 @@
 "use client";
 
-import { getJSONData } from '@/tools/Toolkit';
-import { Orders, Order } from '@/tools/orders.model';
+import { useEffect, useState } from "react";
+import { getJSONData } from "@/tools/Toolkit";
+import { Orders, Order } from "@/tools/orders.model";
 
-export default function OrdersReport() {
+export default function OrdersReport({
+    setAppState,
+    appState,
+}: {
+    setAppState: Function;
+    appState: number;
+}) {
     // retrieve server sided script
-    const RETRIEVE_SCRIPT:string = "https://www.seanmorrow.ca/_lessons/retrieveOrder.php";
-  
+    const RETRIEVE_SCRIPT: string =
+        "https://www.seanmorrow.ca/_lessons/retrieveOrder.php";
 
-    return (<>No orders retrieved...</>);
+    // ------------- private methods
+    const getOrders = async () => {
+        const data: Orders = await getJSONData(RETRIEVE_SCRIPT, false, true);
+        console.log(data);
+        // save it in a state variable - because it is used in JSX and needs to persist
+        setOrders(data.orders);
+
+        // data all loaded, change state of web app
+        setAppState(3);
+    };
+
+    // ------------- use effects
+    useEffect(() => {
+        if (appState == 2) getOrders();
+    }, [appState]);
+
+    // ----------------------------- state variables
+    const [orders, setOrders] = useState<Order[]>([]);
+
+    if (appState == 1) {
+        return <>No orders retrieved...</>;
+    } else if (appState == 3) {
+        return <p>Render orders here</p>;
+    }
 }
